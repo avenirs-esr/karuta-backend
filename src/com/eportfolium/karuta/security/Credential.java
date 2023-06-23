d7d10b0d8ca8875a8670e56c66633cfe02fa4292
GRIBONVALD Julien <julien.gribonvald@recia.fr>
2023-06-23 17:47:21 +0200

    * Making reset usuable by any person that has a right in portfolio . It is so external persons can also reset . Alternative would be adding more definition when adding external persons, if there's problems with this solution

--- src/com/eportfolium/karuta/security/Credential.java
+++ src/com/eportfolium/karuta/security/Credential.java
@@ -1448,5 +1448,31 @@ public class Credential {
       }
       return -1;
   }
+  
+    public boolean hasRightInPortfolio( Connection c, int userId, String nodeUuid ) {
+    	if( nodeUuid == null )
+    		return false;
+    	
+      ResultSet rs = null;
+      PreparedStatement stmt = null;
+      try {
+          String query = "SELECT gri.grid " +
+                  "FROM group_user gu, group_info gi, group_right_info gri, node n " +
+                  "WHERE gu.userid=? AND gu.gid=gi.gid AND gri.grid=gi.grid AND " +
+                  "gri.portfolio_id=n.portfolio_id AND n.node_uuid=uuid2bin(?);";
+          stmt = c.prepareStatement(query);
+          stmt.setInt(1, userId);
+          stmt.setString(2, nodeUuid);
+          rs = stmt.executeQuery();
+
+          if (rs.next())
+              return true;
+      } catch (SQLException e) {
+          logger.error("Managed Error:", e);
+          return false;
+      }
+    	
+    	return false;
+  }
 
 }
